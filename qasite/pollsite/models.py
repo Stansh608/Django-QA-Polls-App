7afee88dbc486526d5ee665a74df55c2383fdcd6
Stansh608 <stanleymunene56@gmail.com>
2023-06-27 12:32:45 +0300

    Phase 1 complete

--- qasite/pollsite/models.py
+++ qasite/pollsite/models.py
@@ -1,3 +1,23 @@
 from django.db import models
-
+from django.utils import timezone
+import datetime
 # Create your models here.
+
+class Question(models.Model):
+    question_txt=models.CharField(("Question Text"), max_length=500) # Question Text will be the label on Django admin site once this model is registered
+    pub_date=models.DateTimeField(("Date Published"))
+    
+    def __str__(self):
+        return self.question_txt
+    
+    def recently_added(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1) #this takes the current time and subtracts a day. Then compares whether the question
+                                                                            #has been posted from that time to recent.
+                                                                            
+class Answer(models.Model):
+    question=models.ForeignKey(Question, on_delete=models.CASCADE) # Relationship, association
+    answer_txt=models.CharField(max_length=100)
+    votes=models.IntegerField(default=0)
+    
+    def __str__(self):
+        return self.answer_txt # Once the answer is added, return its txt
--- qasite/pollsite/views.py
+++ qasite/pollsite/views.py
@@ -1,7 +1,52 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from django.views import generic
+from django.db.models import F
+
+# import the models
+from .models import Question, Answer
 
 
 # Create your views here.
-def index(request):
-    return HttpResponse("Hello There")
+
+#Using generic views
+class IndexView(generic.ListView):
+    template_name="pollsite/index.html"
+    context_object_name= "latest_quiz_list"
+    
+    def get_queryset(self):
+        return Question.objects.order_by('-pub_date')
+    
+    
+#details of the Question
+class DetailView(generic.DetailView):
+    model=Question
+    template_name="pollsite/detail.html"
+    
+
+# vote Action
+def Vote(request, question_id):
+    question=get_object_or_404(Question, pk=question_id)
+    try:
+        selected_ans=question.answer_set.get(pk=request.POST['ans'])
+    except(KeyError, Answer.DoesNotExist):
+        return render(
+            request,
+            "pollsite/detail.html",
+            {
+                "question": question,
+                "error_message": "You did not select an Answer",
+            }
+        )
+        
+    selected_ans.votes=F('votes') + 1
+    selected_ans.save()
+    return HttpResponseRedirect(reverse("pollsite:results", args=(question.id,))) # Redirect to results page 
+        
+
+#Results
+class ResultsView(generic.DetailView):
+    model= Question
+    template_name= "pollsite/result.html"
+    
